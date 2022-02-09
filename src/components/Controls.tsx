e1d0021e4b6cc70915ad58c2bdbae18293a1f846
Dmitry Weiner <dmitry.weiner@gmail.com>
2022-02-09 12:14:04 +0700

    Use keys

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,3 +1,5 @@
+import { useKeyPressEvent } from "react-use";
+
 import {USER_ACTION} from "../App";
 //import styles from "./Controls.module.css";
 
@@ -6,6 +8,11 @@ export type ControlsProps = {
 }
 
 export default function Controls({onChange}: ControlsProps) {
+  useKeyPressEvent("ArrowLeft", () => onChange(USER_ACTION.MOVE_LEFT));
+  useKeyPressEvent("ArrowRight", () => onChange(USER_ACTION.MOVE_RIGHT));
+  useKeyPressEvent("ArrowDown", () => onChange(USER_ACTION.MOVE_DOWN));
+  useKeyPressEvent("ArrowUp", () => onChange(USER_ACTION.TURNING));
+
   return <>
     <button onClick={() => onChange(USER_ACTION.MOVE_LEFT)}>←</button>
     <button onClick={() => onChange(USER_ACTION.TURNING)}>↻</button>
