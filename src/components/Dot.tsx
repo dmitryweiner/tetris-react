55dd94cb68c3de9d2d66ce41edd763429832db91
Dmitry Weiner <dmitry.weiner@gmail.com>
2022-02-09 13:26:09 +0700

    Deploy

--- src/components/Dot.tsx
+++ src/components/Dot.tsx
@@ -1,4 +1,4 @@
-import { BAR, EMPTY_FIELD, FigureId, L_SHAPE, S_SHAPE, SQUARE, TRI } from "../helpers";
+import { BAR, EMPTY_FIELD, L_SHAPE, S_SHAPE, SQUARE, TRI } from "../helpers";
 import styles from "./Dot.module.css";
 
 export type DotProps = {
